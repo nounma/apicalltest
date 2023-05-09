49fe6c222ba7ce2de559a87ccedc62b96b91b05c
Manon Champredon <mah-non@hotmail.com>
2023-05-09 22:04:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if st.button('Search'):
     }
 
     response = requests.get(url, headers=headers, params=querystring)
-    streamingInfo = response.json()["result"][0].get(country, {})["streamingInfo"]
+    streamingInfo = response.json()["result"][0][f"{country}"]["streamingInfo"]
     streamingInfo_first_valie = list(streamingInfo.values())[0]
     title_ok = response.json()["result"][0]["title"]
     overview = response.json()["result"][0]["overview"]
